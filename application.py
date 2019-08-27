93e713cf995c39e8fc355b82727a03344e795df1
Duncan06 <tylerdownie0@gmail.com>
2019-08-27 11:20:19 -0700

    made chatroom selection, but need to display

--- application.py
+++ application.py
@@ -16,26 +16,46 @@ usernames = []
 chat = {}
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if 'username' in session:
-        username = session["username"]
-        return redirect(url_for('chat'))
-    if request.method == "POST":
-        username = request.form.get("username")
-        if username in usernames:
-            return render_template("error.html", message="Username already taken.")
-        usernames.append(username)
-        session['username'] = username
-        return redirect(url_for('chat'))
+    if "username" in session:
+        if "chatid" in session:
+            if len(channels) >= session["chatid"]:
+                return redirect(url_for('chat'))
+        return redirect(url_for('chatrooms'))
     return render_template("index.html")
 
 @app.route("/chatroom", methods=["GET", "POST"])
 def chatrooms():
     if request.method == "POST":
+        username = request.form.get("username")
+        if username in usernames:
+            return render_template("error.html", message="Username already taken.")
+        usernames.append(username)
+        session["username"] = username
 
-@app.route("/chat", methods=["GET", "POST"])
-def chat():
+    if request.method == "GET":
+        return render_template("error.html", message="Please login.")
+
+    return render_template("chatrooms.html", channels = channels, username=session["username"])
+
+@app.route("/chat/<int:chatid>", methods=["GET", "POST"])
+def chat(chatid):
     if request.method == "POST":
-        return jsonify(chat)
+        channel_name = request.form.get("channel_name")
+        if channel_name in channels:
+            render_template("error.html", message="Error, channel name already exists.")
+
+        channels.append(channel_name)
+        chatdict[channel_name] = []
+
+    if request.method == "GET":
+        if username not in session:
+            render_template("error.html", message="Please sign in.")
+        if len(channels) < chatid:
+            render_template("error.html", message="Invalid channel.")
+
+    session["chatid"] = chatid
+
+    render_template("chat.html", username=session["username"])
 
 @socketio.on("message")
 def chatroom(data):
